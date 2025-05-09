705b9edf6bfba0c79091b0f17dc0f2faad98ceea
Stacey jepchumba <satceyjepchumbarr@gamil.com>
2025-05-09 15:21:43 +0300

    conplete

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet("Naureen")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default()
+greet_with_default("Sunny")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+sum_of_numbers= add(1,2)
+print(sum_of_numbers)
+
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None  # Returns None if the number is not an int or float
+    return number / 2
+
+result1 = halve(4)
+print(result1)  
+
+result2 = halve("two")
+print(result2)  
